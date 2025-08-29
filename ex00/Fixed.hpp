69066cd272cb7a6fb2f61857111cf0eca897b593
Yunus Emre Can Sümeralp <emresmrlp@gmail.com>
2025-08-30 00:15:13 +0300

    feat: Fixed.hpp Fixed class init

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ysumeral <ysumeral@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 23:54:17 by ysumeral          #+#    #+#             */
-/*   Updated: 2025/08/29 23:54:34 by ysumeral         ###   ########.fr       */
+/*   Updated: 2025/08/30 00:14:52 by ysumeral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 class Fixed
 {
-
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+	~Fixed();
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+private:
+	int					numberValue;
+	static const int	fractionalBits = 8;
 };
